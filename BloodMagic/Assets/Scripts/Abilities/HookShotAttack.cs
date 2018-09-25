bf8b4b78e0610ff7b976783493b5a6d0dba4980f
hinghing15 <huang.kathy.15@gmail.com>
2018-09-24 21:03:14 -0600

    Start restructuring and refining enemy code

--- BloodMagic/Assets/Scripts/Abilities/HookShotAttack.cs
+++ BloodMagic/Assets/Scripts/Abilities/HookShotAttack.cs
@@ -9,6 +9,7 @@ public class HookShotAttack : SkillClass {
     public float pullSpeed = 25.0f;
     public float maxLength = 10f;
     public float endDistance = 1.4f;
+    //public GameObject hookSpriteGO; 
 
     private Rigidbody2D hook;
     private PlayerController playerController;
@@ -17,11 +18,14 @@ public class HookShotAttack : SkillClass {
     private int hookCount = 0;
     private int hookLength;
     private Vector2 originalHookPos;
+    private Vector2 freezePosition;
     private float prevDist;
     private int frameNum = 0;
     private bool isEnemy;
     private Vector2 hitPoint;
     private Vector3 direction;
+    private GameObject hookSprite;
+    private float distance;
 
     public void Start()
     {
@@ -32,11 +36,12 @@ public class HookShotAttack : SkillClass {
 
     void Update ()
     {
-		if(Input.GetMouseButtonDown(1) && hookCount == 0)
+        if (Input.GetMouseButtonDown(1) && hookCount == 0)
         {
             UseAbility();
+            freezePosition = transform.position;
         }
-	}
+    }
 
     void LateUpdate()
     {
@@ -45,13 +50,20 @@ public class HookShotAttack : SkillClass {
             //lets player hook on for hook time limit
             timeLeft -= Time.deltaTime;
 
+            //hookSprite.transform.position = transform.position;
+            //float scaleX = Mathf.Abs(transform.position.x - hitPoint.x);
+            //hookSprite.transform.localScale = new Vector3(scaleX / 4, 1, 1);
+
+            
             //draw hook line
             lineRenderer.enabled = true;
             lineRenderer.positionCount = 2;
             lineRenderer.SetPosition(0, transform.position);
             if (isEnemy)
                 hitPoint = hook.transform.position;
+
             lineRenderer.SetPosition(1, hitPoint);
+            //hookSprite.transform.Rotate(hitPoint - (Vector2)hookSprite.transform.position);
 
             if (frameNum == 1)
                 pullHook();
@@ -85,9 +97,12 @@ public class HookShotAttack : SkillClass {
         //if ray hits enemy collider, set up hook
         if(hit.collider != null)
         {
-            float distance = Vector2.Distance(hit.point, transform.position);
+            distance = Vector2.Distance(hit.point, transform.position);
+
             if (distance <= maxLength)
             {
+                //createHookSprite();
+                Destroy(hookSprite, hookTimeLimit);
                 hook = hit.rigidbody;
                 if (hit.rigidbody.gameObject.tag == "Enemy")
                 {
@@ -114,6 +129,7 @@ public class HookShotAttack : SkillClass {
             else
             {
                 hook = null;
+                distance = maxLength;
             }
         }
     }
@@ -124,7 +140,10 @@ public class HookShotAttack : SkillClass {
         {//if moving away from enemy, pull enemy to player
             float currDist = Vector2.Distance(originalHookPos, transform.position);
             if (currDist > prevDist)
+            {
+                transform.position = freezePosition; //stop player movement
                 hook.transform.position = Vector3.MoveTowards(hook.transform.position, transform.position + (direction.normalized * endDistance), pullSpeed * 1.3f * Time.deltaTime);
+            }
             //if moving toward enemy, pull player to enemy
             else
             {
@@ -136,5 +155,13 @@ public class HookShotAttack : SkillClass {
         {
             transform.position = Vector3.MoveTowards(transform.position, hitPoint, pullSpeed * Time.deltaTime);
         }
+    }
+
+    void createHookSprite()
+    {
+        /*hookSprite = Instantiate(hookSpriteGO, gameObject.transform.position, gameObject.transform.rotation);
+        hookSprite.transform.Rotate(0, 0, 90);
+        hookSprite.transform.position = transform.position;
+        hookSprite.transform.localScale = new Vector3(0, 1, 1);*/
     }
 }
